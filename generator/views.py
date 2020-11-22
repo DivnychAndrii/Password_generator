89120c5cf8d01041ab5d3c5ba444270d80e8806a
Floyd <floyd@Floyds-iMac.local>
2020-11-22 21:23:59 +0200

    Changing the logic

--- generator/views.py
+++ generator/views.py
@@ -2,25 +2,12 @@ from django.shortcuts import render
 import random
 
 
-# Create your views here.
-
-
 def home(request):
-    return render(request, 'generator/home.html')
-
-
-def about(request):
-    return render(request, 'generator/about.html')
-
-
-def password(request):
 
     the_password = ''
     characters = list('abcdefghijklmnopqrstuvwxyz')
     numbers = list('123456789')
     special = list('!@#$%^&*()')
-
-    length = int(request.GET.get('length', 12))
 
     if request.GET.get('uppercase'):
         characters.extend([x.upper() for x in characters])
@@ -31,7 +18,14 @@ def password(request):
     if request.GET.get('special'):
         characters.extend(special)
 
+    length = int(request.GET.get('length', 12))
     for x in range(length):
         the_password += random.choice(characters)
 
-    return render(request, 'generator/password.html', {'password': the_password})
+    return render(request, 'generator/home.html', {'password': the_password})
+
+
+def about(request):
+    return render(request, 'generator/about.html')
+
+
--- pass_generator/urls.py
+++ pass_generator/urls.py
@@ -18,7 +18,6 @@ from generator import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('password/', views.password, name='password'),
     path('about/', views.about, name='about'),
 
 ]
